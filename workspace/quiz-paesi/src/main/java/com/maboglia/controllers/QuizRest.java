d8b3b9f80a8a28407e4ffd7ce437bb1ae5814da8
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2024-06-19 13:14:15 +0200

    quiz

--- workspace/quiz-paesi/src/main/java/com/maboglia/controllers/QuizRest.java
+++ workspace/quiz-paesi/src/main/java/com/maboglia/controllers/QuizRest.java
@@ -19,6 +19,11 @@ public class QuizRest {
 	@Autowired
 	private QuizService service;
 	
+	@GetMapping("paesi/all")
+	public ResponseEntity<List<Paese>> getPaesi(){
+		return new ResponseEntity<>(service.getPaesiRandom(90), HttpStatus.OK);
+	}
+	
 	@GetMapping("paesi")
 	public ResponseEntity<List<Paese>> getPaesiRandom(){
 		return new ResponseEntity<>(service.getPaesiRandom(3), HttpStatus.OK);
